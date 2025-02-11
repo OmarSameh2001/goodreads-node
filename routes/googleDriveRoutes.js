07f55c7eedbeb2bb43e3b51a979f4c439a3d6201
Aishaa205 <aishaamr54@gmail.com>
2025-02-11 16:24:51 +0200

    fix google drive

--- routes/googleDriveRoutes.js
+++ routes/googleDriveRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
 const googleDriveController = require("../controllers/googleDrivecontroller");
 
+
+
+const uploadDir = path.join(__dirname, "../uploads_files");
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 //storege b destination mo3ayan w link
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./uploads_files");
+        cb(null,uploadDir);// "./uploads_files");
     }, 
     filename: function (req, file, cb) {
          const uniqueSuffix = Date.now() 
